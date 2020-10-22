8170cd07be835d2c25af63be3224677f6d0ddf8c
홍 소영 <r2d2_hong@domain.aispera.com>
2020-10-22 20:23:22 +0900

    Updates

---
+++ precache-manifest.83773cc3b7b05df26d4ef34a361deb22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d0c93358728eec4d105861be1ff39d",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "845f06a4935ada44ea4a",
+    "url": "/movie_app_2020/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "f575d44c1b0e9225b766",
+    "url": "/movie_app_2020/static/js/2.122f7658.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app_2020/static/js/2.122f7658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845f06a4935ada44ea4a",
+    "url": "/movie_app_2020/static/js/main.81a2a788.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
